ffa6a45b49536d8a46ac62f6355e57751562776b
Pra9ptl <patelpranav1313@gmail.com>
2020-04-12 15:10:17 -0400

    error handling if any error occurs

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -11,7 +11,8 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     postSelectedHandler = (id) => {
@@ -29,34 +30,40 @@ class Blog extends Component {
             });
             this.setState({ posts: updatedPosts });
             // console.log(response);
-        });
+        })
+            .catch(error => {
+                console.log(error);
+                this.setState({ error: true });
+            });
     }
 
     render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!!!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(
+                post => {
+                    return <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)} />
+                }
+            );
+        }
 
-        const posts = this.state.posts.map(
-            post => {
-                return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author} 
-                clicked={() => this.postSelectedHandler(post.id)} />
-            }
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId} />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
         );
-
-return (
-    <div>
-        <section className="Posts">
-            {posts}
-        </section>
-        <section>
-            <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-            <NewPost />
-        </section>
-    </div>
-);
     }
 }
 
