7e404150a870ee9456122333364772aa470e2d66
Pra9ptl <patelpranav1313@gmail.com>
2020-04-14 12:36:19 -0400

    Nested routing in multiple component

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Axios from '../../axios';
 import Post from '../../components/Post/Post';
 import './Posts.css';
-// import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import FullPost from '../FullPost/FullPost';
 
 class Posts extends Component {
     state = {
@@ -10,7 +11,7 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.props.history.push({ pathname: "/" + id });
+        this.props.history.push({ pathname: "/posts/" + id });
     }
 
     componentDidMount() {
@@ -39,7 +40,7 @@ class Posts extends Component {
             posts = this.state.posts.map(
                 post => {
                     return (
-                        // <Link to={ '/' + post.id } key={post.id}>
+                        // <Link to={ '/posts/' + post.id } key={post.id}>
                         <Post
                             key={post.id}
                             title={post.title}
@@ -52,9 +53,12 @@ class Posts extends Component {
         }
 
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path= {this.props.match.url + "/:id"} exact component={FullPost} />
+            </div>
         );
     }
 }
